82d815f0ec1fea41b888234d13d598f232ea675a
Ashnita Bali <ashnita01@gmail.com>
2014-07-05 22:31:42 +0100

    first commit

---
+++ js/do-slide.js
@@ -0,0 +1,27 @@
+/**
+ * Created by ashnita on 24/03/2014.
+ */
+$(function() {
+    $('#slides').slidesjs({
+        height: 235,
+        navigation: false,
+        pagination: false,
+        effect: {
+            fade: {
+                speed: 400}},callback:
+        {
+            start: function(number)
+            {
+                $("#slider_content1,#slider_content2,#slider_content3").fadeOut(500);},
+            complete: function(number)
+            {
+                $("#slider_content" + number).delay(500).fadeIn(1000);}},
+        play:
+        {
+            active: false,
+            auto: true,
+            interval: 6000,
+            pauseOnHover: false
+        }
+    });
+});
